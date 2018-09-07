ab34f6701b0f116874f8a7d78e698236668cd4df
juan carlos <hhjuan27@gmail.com>
2018-09-07 12:59:00 -0500

    index
    archivo index necesario para el inicio

---
+++ views/index.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<style>
+
+</style>
+<body>
+	<div id="content" class="col-lg-10 col-sm-10">
+		<?php 
+		##formulario para la busqueda y el listado paginado
+		echo $FormularioBusqueda; 
+		?>
+	</div>
+</body>
+</html>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="<?php echo base_url();?>js/search_to_list.js"></script>
